059c603055a216f6268235b6ad222a80af51b625
manu <somamanojshm.22@gmail.com>
2024-12-05 15:13:22 +0530

    Changes were made on Add,java

--- Add.java
+++ Add.java
@@ -4,7 +4,7 @@ public class Add {
     public static void main(String[] args) {
         int a = 20;
         int b = 25;
-        System.out.println(a + b);
+        System.out.println(a * b);
 
     }
 }
